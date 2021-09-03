0d6d496c5b00a829ea0b1625e58a75d6ca96d654
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-03 18:52:30 +0300

    modified the user_test.py

--- user.py
+++ user.py
@@ -2,9 +2,26 @@ class User:
     '''
     generates new instances of password lock
     '''
-    user_list = [] #empty passwordlocker
+    user_lock = [] #empty passwordlocker
 
     def __init__(self, username,password):
 
         self.username = username
         self.password = password
+
+    def save_user(self):
+        '''
+        save user password lock'''
+        User.user_lock.append(self)
+
+    def delete_user(self):
+        User.user_lock.remove(self)
+
+    @classmethod
+    def find_by_password(cls, password):
+       for user in cls.user_lock:
+           if user.password == password:
+               return user
+    
+
+   
--- user_test.py
+++ user_test.py
@@ -12,8 +12,39 @@ class TestUser(unittest.TestCase):
 
     def test_save_user(self):
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1) 
-        
-           
+        self.assertEqual(len(User.user_lock),1) 
+
+    def tearDown(self):
+
+        '''
+        clean up afer each testcase run
+        '''
+        User.user_list = []
+    def test_save_multilpe_user(self):
+        self.new_user.save_user() 
+        test_user = User("Maureen","3344") 
+        test_user.save_user() 
+        self.assertEqual(len(User.user_lock),2) 
+
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("Maureen","3344")
+
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_lock),1)
+
+
+    def test_find_user_by_password(self):
+        self.new_user.save_user()
+        test_user= User("Maureen","3344")
+        test_user.save_user()
+        found_user = User.find_by_password("3344")
+        self.assertEqual(found_user.username,test_user.username)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
