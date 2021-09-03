8d52d72fa940efc9cec1c1a40878c7cb780e624f
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-03 12:10:43 +0300

    have added user_test.py file

---
+++ user.py
@@ -0,0 +1,10 @@
+class user:
+    '''
+    generates new instances of password lock
+    '''
+    user_list = [] #empty passwordlocker
+
+    def __init__(self, username,password):
+
+        self.username = username
+        self.password = password
