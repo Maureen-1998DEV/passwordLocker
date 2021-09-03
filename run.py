0528a5081644f4d325aa49d42cbba0788a5d1e45
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-03 21:27:11 +0300

    Added menu in run.py

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-from user import User
+
 menu=""
 while menu != '1'or menu != '2':
-    menu = input("Would you like to sava a new password or iew old ones"
+    menu = input("Would you like to save a new password or iew old ones?"
     "\n1. input new password"
     "\n2. view password"
      "\n3. Exit")
-    if menu=='1 ':
+    if menu=='1':
        software =input("Enter your software name:")
        username = input("Enter your username:")
-       password = input("Enter your password")
+       password = input("Enter your password:")
 
-    if menu == "2":
+    if menu == '2':
         print("display passwords")
 
     if menu=='3':
